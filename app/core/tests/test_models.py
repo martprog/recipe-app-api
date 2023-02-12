b885331e81584258f893bc4d3d9fa2c88067b04c
martprog <martinscaraguel@gmail.com>
2023-02-12 18:31:24 +0100

    additional test units and django admin setup

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -23,3 +23,15 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'password123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'password123')
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'email@example.com',
+            'password123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
